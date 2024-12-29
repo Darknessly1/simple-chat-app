862feffb8808d707a826bc25dea56693cae1702a
Amine lehkim <74158555+Darknessly1@users.noreply.github.com>
2024-12-29 17:48:13 +0100

    Frontend Completed

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import { BiLogOut } from "react-icons/bi"
+
+const LogoutButton = () => {
+    return (
+        <div className="mt-auto">
+            <BiLogOut />
+        </div>
+    )
+}
+
+export default LogoutButton
